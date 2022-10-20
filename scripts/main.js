a823af8d3f71de132081fe2478167992f6457948
Shubham Kumar <61176681+helloshubhamkumar@users.noreply.github.com>
2022-10-20 15:37:41 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
+function random(start, end) {
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+}
+
+function changeColor() {
+    count++;
+    const rndColor = `rgb(${random(0,255)}, ${random(0,255)}, ${random(0,255)})`;
+    document.body.style.backgroundColor = rndColor;
+    if (count === 10) {
+      clearInterval(interval);
+    }
+  
+}
+
+let count = 0;
+let interval = setInterval(changeColor, 150);
+
 
